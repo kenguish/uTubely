14e2c0b9710f4e133afaff13ed50fb97bad9d854
Kenneth Anguish <kenguish@gmail.com>
2012-09-09 13:16:51 +0800

    Commite change

--- uTubely/AppDelegate.m
+++ uTubely/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "JSONKit.h"
+#import "HCYoutubeParser.h"
 
 @implementation AppDelegate
 @synthesize queue, urlTextField, downloadButton, connection, buffer, extractedURL;
@@ -19,7 +20,7 @@
     queue = [[ASINetworkQueue alloc] init];
     
     // Insert code here to initialize your application
-    [self.urlTextField setStringValue: @"http://www.youtube.com/watch?v=QH2-TGUlwu4"];
+    [self.urlTextField setStringValue: @"http://www.youtube.com/watch?v=zaOn1u8wIT0&feature=related"];
     [self.statusTextField setStringValue: @"Click \"Start\""];
     [self.progressIndicator setIndeterminate: NO];
     [self.progressIndicator setDoubleValue: 0.0];
@@ -52,7 +53,7 @@
         downloadingFlag = NO;
         
     } else {
-        [self.statusTextField setStringValue: @"Loading the Youtube page."];
+        [self.statusTextField setStringValue: @"Loading the Youtube URL."];
         
         // stop movie first
         
@@ -62,138 +63,104 @@
             self.qtView.movie = nil;
         }
         
-        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:
-                                        
-                                        [NSURL URLWithString: [self.urlTextField stringValue]]
-                                        ];
-        [request setValue:(NSString *)kUserAgent forHTTPHeaderField:@"User-Agent"];
-
-        self.connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
-        [self.connection start];
-        
         downloadingFlag = YES;
-    }
-}
 
-
-#pragma mark -
-#pragma mark NSURLConnectionDelegate
-
--(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
-    NSUInteger capacity;
-    if (response.expectedContentLength != NSURLResponseUnknownLength) {
-        capacity = response.expectedContentLength;
-    }
-    else {
-        capacity = 0;
-    }
-    
-    self.buffer = [[NSMutableData alloc] initWithCapacity:capacity];
-}
-
-
--(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-    [self.buffer appendData:data];
-}
-
-
-
--(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
-    [self _closeConnection];
-    
-    [self.statusTextField setStringValue: @"Unable to download."];
-    downloadingFlag = NO;
-}
--(void)_closeConnection {
-    [self.connection cancel];
-    self.connection = nil;
-    self.buffer = nil;
-}
-
--(void)connectionDidFinishLoading:(NSURLConnection *) connection {
-    NSString* html = [[NSString alloc] initWithData: self.buffer encoding:NSUTF8StringEncoding];
-    [self _closeConnection];
-    
-    //NSLog(@"html: %@", html);
-    
-    if (html.length <= 0) {
-        [self.statusTextField setStringValue: @"URL might be invalid."];
-        downloadingFlag = NO;
         
-        return;
-    }
-    
-    NSError* error = nil;
-    
-    self.extractedURL = nil;
-    self.extractedURL = [self _extractYouTubeURLFromFile:html error:&error];
-    if (error) {
-        [self.statusTextField setStringValue: @"URL might be invalid."];
-        downloadingFlag = NO;
+        NSDictionary *videos = [HCYoutubeParser h264videosWithYoutubeURL:[NSURL URLWithString: [self.urlTextField stringValue]]];
         
-    } else {
-        downloadingFlag = YES;
-        
-        //[self _didSuccessfullyExtractYouTubeURL:self.extractedURL];
-        //[self _loadVideoWithContentOfURL:self.extractedURL];
-        
-        [self.statusTextField setStringValue: @"Downloading..."];
-        
-        if ( self.extractedURL != nil ) {
-            [self.progressIndicator setIndeterminate: NO];
+        if (videos != nil) {
+            // fetch hd720 first?
+            NSString *url = [videos objectForKey: @"hd720"];
             
-            [self.downloadButton setTitle: @"Stop!"];
-            
-            NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
-            [dateFormat setDateFormat:@"yyyy-MM-dd-hh-mma"];
-            NSString *dateString = [dateFormat stringFromDate: [NSDate date]];
-            [dateFormat release];
-            
-            NSString *randomFilename = [NSString stringWithFormat: @"download_%@", dateString];
-            
-            r = [ASIHTTPRequest requestWithURL: self.extractedURL];
-
-            NSString *local_full_path = [ [NSString stringWithFormat: @"~/Desktop/%@.mp4", randomFilename] stringByExpandingTildeInPath];
-            //NSLog(@"Saving to: %@", local_full_path);
-            //NSLog(@"Saving to the parent folder: %@", [local_full_path stringByDeletingLastPathComponent]);
-            [r setTimeOutSeconds: 10];
-            
-            [r setAllowResumeForFileDownloads: YES];
-            [r setNumberOfTimesToRetryOnTimeout:0];
-            [r setDownloadDestinationPath: local_full_path];
-            [r setTemporaryFileDownloadPath: [NSString stringWithFormat: @"%@.download", local_full_path]];
-            [r setDownloadProgressDelegate: self.progressIndicator];
-            
-            [r setShowAccurateProgress:YES];
-            [r setDelegate: self];
-            
-            [r setCompletionBlock:^{
-                [self.statusTextField setStringValue: @"Download completed."];
+            if (url == nil) {
+                url = [videos objectForKey: @"medium"];
                 
-                self.extractedURL = nil;
-                [self.progressIndicator setIndeterminate: YES];
-                
-                // play movie
-                NSError *error = nil;
-                QTMovie *movie = [[QTMovie alloc] initWithFile: [ [NSString stringWithFormat: @"~/Desktop/%@.mp4", randomFilename] stringByExpandingTildeInPath] error: &error];
-                if (error == nil) {
-                    [self.qtView setMovie: movie];
+                if (url == nil) {
+                    url = [videos objectForKey: @"small"];
                     
-                    [movie release];
-                    [[self.qtView movie] play];
-                    
-                    //[self.qtView play: nil];
+                    [self.statusTextField setStringValue: @"Downloading the Youtube URL in small format"];
+
                 } else {
-                    NSLog(@"Unable to play video");
+                    
+                    [self.statusTextField setStringValue: @"Downloading the Youtube URL in medium format"];
+
                 }
+            } else {
+                [self.statusTextField setStringValue: @"Downloading the Youtube URL in HD720 format"];
+
+            }
+            
+            if (url != nil) {
+                self.extractedURL = [NSURL URLWithString: url];
+                // start fetching videos
+                [self.progressIndicator setIndeterminate: NO];
                 
-                [self.downloadButton setTitle: @"Go!"];
+                [self.downloadButton setTitle: @"Stop!"];
                 
-                downloadingFlag = NO;
-            }];
-        
-            [r setFailedBlock:^{
-                [self.statusTextField setStringValue: @"Download failed."];
+                NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+                [dateFormat setDateFormat:@"yyyy-MM-dd-hh-mma"];
+                NSString *dateString = [dateFormat stringFromDate: [NSDate date]];
+                [dateFormat release];
+                
+                NSString *randomFilename = [NSString stringWithFormat: @"download_%@", dateString];
+                
+                r = [ASIHTTPRequest requestWithURL: self.extractedURL];
+                
+                NSString *local_full_path = [ [NSString stringWithFormat: @"~/Desktop/%@.mp4", randomFilename] stringByExpandingTildeInPath];
+                //NSLog(@"Saving to: %@", local_full_path);
+                //NSLog(@"Saving to the parent folder: %@", [local_full_path stringByDeletingLastPathComponent]);
+                [r setTimeOutSeconds: 10];
+                
+                [r setAllowResumeForFileDownloads: YES];
+                [r setNumberOfTimesToRetryOnTimeout:0];
+                [r setDownloadDestinationPath: local_full_path];
+                [r setTemporaryFileDownloadPath: [NSString stringWithFormat: @"%@.download", local_full_path]];
+                [r setDownloadProgressDelegate: self.progressIndicator];
+                
+                [r setShowAccurateProgress:YES];
+                [r setDelegate: self];
+                
+                [r setCompletionBlock:^{
+                    [self.statusTextField setStringValue: @"Download completed."];
+                    
+                    self.extractedURL = nil;
+                    [self.progressIndicator setIndeterminate: YES];
+                    
+                    // play movie
+                    NSError *error = nil;
+                    QTMovie *movie = [[QTMovie alloc] initWithFile: [ [NSString stringWithFormat: @"~/Desktop/%@.mp4", randomFilename] stringByExpandingTildeInPath] error: &error];
+                    if (error == nil) {
+                        [self.qtView setMovie: movie];
+                        
+                        [movie release];
+                        [[self.qtView movie] play];
+                        
+                        //[self.qtView play: nil];
+                    } else {
+                        NSLog(@"Unable to play video");
+                    }
+                    
+                    [self.downloadButton setTitle: @"Go!"];
+                    
+                    downloadingFlag = NO;
+                }];
+                
+                [r setFailedBlock:^{
+                    [self.statusTextField setStringValue: @"Download failed."];
+                    
+                    // unset the current updateset
+                    self.extractedURL = nil;
+                    [self.progressIndicator setIndeterminate: YES];
+                    
+                    [self.downloadButton setTitle: @"Go!"];
+                    
+                    downloadingFlag = NO;
+                }];
+                
+                [r startAsynchronous];
+
+            } else {
+                [self.statusTextField setStringValue: @"Unable to find any Youtube videos with the URL"];
                 
                 // unset the current updateset
                 self.extractedURL = nil;
@@ -202,122 +169,27 @@
                 [self.downloadButton setTitle: @"Go!"];
                 
                 downloadingFlag = NO;
-             }];
+            }
+        } else {
+            [self.statusTextField setStringValue: @"Unable to find any Youtube videos with the URL"];
             
-            [r startAsynchronous];
+            // unset the current updateset
+            self.extractedURL = nil;
+            [self.progressIndicator setIndeterminate: YES];
+            
+            [self.downloadButton setTitle: @"Go!"];
+            
+            downloadingFlag = NO;
         }
     }
 }
+
 
 - (void)updateProgress {
     //NSLog(@"- (void)updateProgress: %@", [NSString stringWithFormat: @"%.0f%%", [self.progressIndicator doubleValue] * 100.0 ]);
     [self.percentageTextField setStringValue: [NSString stringWithFormat: @"%.0f%%", [self.progressIndicator doubleValue] * 100.0  ]];
 }
 
-
--(NSString *)_unescapeString:(NSString *)string {
-    // will cause trouble if you have "abc\\\\uvw"
-    // \u   --->    \U
-    NSString *esc1 = [string stringByReplacingOccurrencesOfString:@"\\u" withString:@"\\U"];
-    
-    // "    --->    \"
-    NSString *esc2 = [esc1 stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
-    
-    // \\"  --->    \"
-    NSString *esc3 = [esc2 stringByReplacingOccurrencesOfString:@"\\\\\"" withString:@"\\\""];
-    
-    NSString *quoted = [[@"\"" stringByAppendingString:esc3] stringByAppendingString:@"\""];
-    NSData *data = [quoted dataUsingEncoding:NSUTF8StringEncoding];
-    
-    //  NSPropertyListFormat format = 0;
-    //  NSString *errorDescr = nil;
-    NSString *unesc = [NSPropertyListSerialization propertyListFromData:data mutabilityOption:NSPropertyListImmutable format:NULL errorDescription:NULL];
-    
-    if ([unesc isKindOfClass:[NSString class]]) {
-        // \U   --->    \u
-        return [unesc stringByReplacingOccurrencesOfString:@"\\U" withString:@"\\u"];
-    }
-    
-    return nil;
-}
-
-
--(NSURL*)_extractYouTubeURLFromFile:(NSString *)html error:(NSError *__autoreleasing *)error {
-    NSString *JSONStart = nil;
-    NSString *JSONStartFull = @"ls.setItem('PIGGYBACK_DATA', \")]}'";
-    NSString *JSONStartShrunk = [JSONStartFull stringByReplacingOccurrencesOfString:@" " withString:@""];
-    if ([html rangeOfString:JSONStartFull].location != NSNotFound)
-        JSONStart = JSONStartFull;
-    else if ([html rangeOfString:JSONStartShrunk].location != NSNotFound)
-        JSONStart = JSONStartShrunk;
-    
-    if (JSONStart != nil) {
-        NSScanner* scanner = [NSScanner scannerWithString:html];
-        [scanner scanUpToString:JSONStart intoString:nil];
-        [scanner scanString:JSONStart intoString:nil];
-        
-        NSString *JSON = nil;
-        [scanner scanUpToString:@"\");" intoString:&JSON];
-        JSON = [self _unescapeString:JSON];
-        NSError* decodingError = nil;
-        NSDictionary* JSONCode = nil;
-        
-        // First try to invoke NSJSONSerialization (Thanks Mattt Thompson)
-        
-        id NSJSONSerializationClass = NSClassFromString(@"NSJSONSerialization");
-        SEL NSJSONSerializationSelector = NSSelectorFromString(@"dataWithJSONObject:options:error:");
-        if (NSJSONSerializationClass && [NSJSONSerializationClass respondsToSelector:NSJSONSerializationSelector]) {
-            JSONCode = [NSJSONSerialization JSONObjectWithData:[JSON dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingAllowFragments error:&decodingError];
-        }
-        else {
-            JSONCode = [JSON objectFromJSONStringWithParseOptions:JKParseOptionNone error:&decodingError];
-        }
-        
-        if (decodingError) {
-            // Failed
-            
-            *error = decodingError;
-        }
-        else {
-            // Success
-            
-            //NSLog(@"json: %@", [JSONCode objectForKey:@"content"]);
-            
-            NSArray* videos = [[[JSONCode objectForKey:@"content"] objectForKey:@"video"] objectForKey:@"fmt_stream_map"];
-            //NSLog(@"videos: %@", videos);
-            
-            NSString* streamURL = nil;
-            
-            if (videos.count) {
-                NSString* streamURLKey = @"url";
-                
-                if (self.quality == LBYouTubePlayerQualityLarge) {
-                    streamURL = [[videos objectAtIndex:0] objectForKey:streamURLKey];
-                }
-                else if (self.quality == LBYouTubePlayerQualityMedium) {
-                    unsigned int index = MIN(0, videos.count-2);
-                    streamURL = [[videos objectAtIndex:index] objectForKey:streamURLKey];
-                }
-                else {
-                    streamURL = [[videos lastObject] objectForKey:streamURLKey];
-                }
-            }
-            
-            if (streamURL) {
-                return [NSURL URLWithString:streamURL];
-            } else {
-                NSLog(@"Couldn't find the stream URL.");
-            }
-        }
-    }
-    else {
-        //NSLog(@"The JSON data could not be found.");
-        
-        downloadingFlag = NO;
-    }
-    
-    return nil;
-}
 
 - (void)dealloc {
     [queue release], self.queue = nil;
